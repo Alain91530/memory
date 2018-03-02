c706d632a45eed8a3dbfb42f24740af14ef96d3e
Unknown <alain.cadenat@depannum.com>
2018-03-02 22:28:01 +0100

    feat: Adding JS

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/*******************************************************************************
+
+    Variables
+
+*******************************************************************************/
+/* this table represent the desk. It's filled with pairs of values representing
+   the eight pairs of different cards
+*******************************************************************************/
+
+let desk = [1, 1, 2 ,2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+/*******************************************************************************
+
+    Functions
+
+*******************************************************************************/
+
+function shuffleCards(desk) {
+  for (let position = desk.length-1; position > 0; position--){
+    // pick a random position in the desk
+    let randomPosition = Math.floor(Math.random()*(position+1));
+    // swap desk[posion] and desk[rendomPosition]
+    let saveCard = desk[position];
+    desk[position] = desk[randomPosition];
+    desk[randomPosition] = saveCard;
+  };
+};
+
+console.log(desk);
+shuffleCards(desk);
+console.log(desk);
