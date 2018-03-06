1bb961d92d8308c65408a918aa527567e608659e
Unknown <alain.cadenat@depannum.com>
2018-03-06 20:01:54 +0100

    feat: Timer running

--- js/app.js
+++ js/app.js
@@ -4,15 +4,16 @@
 *******************************************************************************/
 
 let timer = 0;
+
 let timerIntervalId = 0;
 let flips = 0;           // A move = 2 flips
 
 /*  Table containing the desk. It's filled with pairs of html elements
     representing the value of the eight pairs of different cards              */
 let desk = document.getElementsByClassName('picture');
-//  Table containing the comple html elements of the cards of the desk
+//  Table containing the complete html elements of the cards of the desk
 let cards = document.getElementsByClassName('card');
-
+let timerScore = document.getElementById('time');
 // Differents stages of the game set as constants to be easy to change.
 const maxFlips = 48;
 const oneStar = 32;
@@ -28,6 +29,32 @@ const play = document.querySelector('#start-over');
 
 function changeTimer() {
   timer++;
+/*
+    Convert the timer in a string with xx:xx:xx format to change de DOM
+                                                                              */
+  let hrs = Math.trunc(timer/3600)+":";
+  let mins = Math.trunc(timer/60)+":";
+  let secs = timer-(parseInt(mins)*60);
+  if ((hrs.length) == 2) {
+    hrs = "0"+hrs;
+  }
+  else {
+    hrs = hrs+":";
+  };
+  if ((mins.length) == 2) {
+    mins = "0"+mins;
+  }
+  else {
+    mins = mins+":";
+  };
+  secs=secs+""
+  if ((secs.length) == 1) {
+    secs = "0"+secs;
+  };
+  console.log(hrs+mins+secs);   // DEBUG only
+  console.log(timerScore);
+//  timerScore=timerScore.querySelector('span');
+  timerScore.querySelector('span').textContent = hrs+mins+secs;
 }
 /*******************************************************************************
     Shuffle the cards
@@ -88,7 +115,7 @@ function startGame() {
   /*
     Reset the timer if any was running and start a new one
                                                                               */
-  timer = 0;
+  timer = -1;
   if(timerIntervalId != 0) {window.clearInterval(timerIntervalId);}
   timerIntervalId = window.setInterval(changeTimer, 1000);
   console.log(desk);            // DEBUG only
